1826fa50f51fe67a8e8bf441e3573855048894c8
Amadox <marcus@amadox.org>
2015-03-06 21:55:12 +0100

    Error Handling for follow()ing non-existing links

--- src/travhaller.js
+++ src/travhaller.js
@@ -4,13 +4,13 @@ var Q = require('q');
  * @param url       an API Url to query
  * @param apiRoot   (optional) the Root of the API, in case it differs from the <url> given
  */
-var createNode = function(url, apiRoot) {
+var createNode = function (url, apiRoot) {
     var _apiRoot = apiRoot || url;
     var _response = {};
     var _resource = {};
     var _data = undefined;
 
-    var promise = Q.Promise(function(resolve, reject, notify) {
+    var promise = Q.Promise(function (resolve, reject, notify) {
         var request = new XMLHttpRequest();
         request.open("GET", url, true);
         request.onload = onSuccess;
@@ -22,49 +22,61 @@ var createNode = function(url, apiRoot) {
             _response = request;
             if (_response.status >= 200 && _response.status < 300) {
                 _resource = JSON.parse(request.responseText);
-                (resolve||function(){})(_resource);
+                (resolve || function () {
+                })(_resource);
             } else {
-                (reject||function(){})(new Error("[Travhaller] API Request to " + url + " failed with Status code " + _response.status));
+                (reject || function () {
+                })(new Error('[Travhaller] API Request to \'' + url + '\' failed with Status code ' + _response.status));
             }
         }
 
         function onError() {
-            (reject||function(){})(new Error("Can't XHR " + JSON.stringify(url)));
+            (reject || function () {
+            })(new Error('Can\'t XHR ' + JSON.stringify(url)));
         }
 
         function onProgress(event) {
-            (notify||function(){})(event.loaded / event.total);
+            (notify || function () {
+            })(event.loaded / event.total);
         }
 
     });
 
     return {
-        then: function(resolve, reject, notify) {
-            return promise.then(function() {
-                (resolve||function(){})({
-                    getApiRoot: function()  {
-                      return _apiRoot;
+        then: function (resolve, reject, notify) {
+            return promise.then(function () {
+                (resolve || function () {
+                })({
+                    getApiRoot: function () {
+                        return _apiRoot;
                     },
 
-                    setApiRoot: function(apiRoot) {
+                    setApiRoot: function (apiRoot) {
                         _apiRoot = apiRoot;
                         return this;
                     },
 
-                    follow: function(linkname) {
-                        var url = _resource._links[linkname].href;
-                        return createNode(_apiRoot + url, _apiRoot);
+                    follow: function (linkname) {
+                        if (_resource._links[linkname] === undefined) {
+                            console.error('[Travhaller] Can\'t follow link \'' + linkname + '\' because it does not exist in current resource.');
+                            return this;
+                        } else {
+                            var url = _resource._links[linkname].href;
+                            return createNode(_apiRoot + url, _apiRoot);
+                        }
                     },
 
-                    getResource: function() {
+                    getResource: function () {
                         return _resource;
                     }
                 });
-            }, function(error) {
+            }, function (error) {
                 console.error(error.message);
-                (reject||function(){})(error);
-            }, function(status) {
-                (notify||function(){})(status);
+                (reject || function () {
+                })(error);
+            }, function (status) {
+                (notify || function () {
+                })(status);
             });
         }
     };
