03021e745785ce5fbacd58343ba9b9e04116df40
Amadox <marcus@amadox.org>
2015-03-14 22:19:01 +0100

    Use of _embedded on follow() if available

--- src/travhaller.js
+++ src/travhaller.js
@@ -2,84 +2,106 @@ var Q = require('q');
 
 /**
  * @param url       an API Url to query
- * @param apiRoot   (optional) the Root of the API, in case it differs from the <url> given
+ * @param rootUrl   (optional) the Root of the API, in case it differs from the <url> given
+ * @return Promise
  */
-var createNode = function (url, apiRoot) {
-    var _apiRoot = apiRoot || url;
+var createTravhallerNodePromise = function (url, rootUrl) {
+    var _rootUrl = rootUrl || url;
     var _response = {};
     var _resource = {};
-    var _data = undefined;
 
-    var promise = Q.Promise(function (resolve, reject, notify) {
+    return Q.Promise(function (resolve, reject) {
+        resolve = (resolve || function() {});
+        reject = (reject || function() {});
+
         var request = new XMLHttpRequest();
         request.open("GET", url, true);
         request.onload = onSuccess;
         request.onerror = onError;
-        request.onprogress = onProgress;
         request.send();
 
         function onSuccess() {
             _response = request;
             if (_response.status >= 200 && _response.status < 300) {
                 _resource = JSON.parse(request.responseText);
-                (resolve || function () {
-                })(_resource);
+                resolve(new TravhallerNode(_rootUrl, _resource));
+
             } else {
-                (reject || function () {
-                })(new Error('[Travhaller] API Request to \'' + url + '\' failed with Status code ' + _response.status));
+                throwError("API Request to '" + url + "' failed with Status code " + _response.status);
             }
         }
 
         function onError() {
-            (reject || function () {
-            })(new Error('Can\'t XHR ' + JSON.stringify(url)));
+            throwError("Can't XHR " + JSON.stringify(url));
         }
 
-        function onProgress(event) {
-            (notify || function () {
-            })(event.loaded / event.total);
+        function throwError(errorMsg) {
+            errorMsg = "[Travhaller] " + errorMsg;
+            console.warn(errorMsg);
+            reject(new Error(errorMsg));
         }
 
     });
-
-    return {
-        then: function (resolve, reject, notify) {
-            return promise.then(function () {
-                (resolve || function () {
-                })({
-                    getApiRoot: function () {
-                        return _apiRoot;
-                    },
-
-                    setApiRoot: function (apiRoot) {
-                        _apiRoot = apiRoot;
-                        return this;
-                    },
-
-                    follow: function (linkname) {
-                        if (_resource._links[linkname] === undefined) {
-                            console.error('[Travhaller] Can\'t follow link \'' + linkname + '\' because it does not exist in current resource.');
-                            return this;
-                        } else {
-                            var url = _resource._links[linkname].href;
-                            return createNode(_apiRoot + url, _apiRoot);
-                        }
-                    },
-
-                    getResource: function () {
-                        return _resource;
-                    }
-                });
-            }, function (error) {
-                console.error(error.message);
-                (reject || function () {
-                })(error);
-            }, function (status) {
-                (notify || function () {
-                })(status);
-            });
-        }
-    };
 };
 
-exports.get = createNode;
+var TravhallerNode = function(rootUrl, resource) {
+    var _rootUrl = rootUrl;
+    var _resource = resource;
+
+    /**
+     * @param linkname
+     * @returns Promise
+     */
+    this.follow = function (linkname) {
+        var embedded = _resource._embedded || {};
+        var links = _resource._links || {};
+
+        // use embedded resource
+        if (embedded[linkname] !== undefined) {
+            return Q.Promise(function(resolve) {
+                resolve(new TravhallerNode(rootUrl, embedded[linkname]));
+            });
+
+            // use link
+        } else if (links[linkname] !== undefined) {
+            var linkUrl = _rootUrl + links[linkname].href;
+            return createTravhallerNodePromise(linkUrl, _rootUrl);
+        } else {
+            return TravhallerError("Can't follow link '" + linkname + "' because it does not exist in current resource.");
+        }
+    };
+
+    /**
+     * @returns {*}
+     */
+    this.getResource = function () {
+        return _resource;
+    };
+
+    this.getRootUrl = function() {
+        return _rootUrl;
+    };
+
+    this.setRootUrl = function(rootUrl) {
+        _rootUrl = rootUrl;
+        return this;
+    };
+
+    this.refresh = function() {
+
+        var url = _resource._links.self.href;
+        return createTravhallerNodePromise(_rootUrl + url, _rootUrl);
+    };
+
+    return this;
+};
+
+var TravhallerError = function(errorMsg) {
+    errorMsg = "[Travhaller] " + errorMsg;
+    console.warn(errorMsg);
+    return Q.Promise(function(resolve, reject){
+        reject(new Error(errorMsg));
+    });
+};
+
+exports.get = createTravhallerNodePromise;
